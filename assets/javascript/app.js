7f468357dbf973fd03edc416bdf86d2987301cf4
Julia Shreckhise <julia.shreckhise@gmail.com>
2018-03-19 23:02:48 -0400

    needs styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,66 +1,68 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyD1MqCxkQf6Nc3T3oUMBDzVdZxoWlM-Dlk",
-    authDomain: "trainschedule-7a996.firebaseapp.com",
-    databaseURL: "https://trainschedule-7a996.firebaseio.com",
-    projectId: "trainschedule-7a996",
-    storageBucket: "trainschedule-7a996.appspot.com",
-    messagingSenderId: "480470890423"
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD1MqCxkQf6Nc3T3oUMBDzVdZxoWlM-Dlk",
+  authDomain: "trainschedule-7a996.firebaseapp.com",
+  databaseURL: "https://trainschedule-7a996.firebaseio.com",
+  projectId: "trainschedule-7a996",
+  storageBucket: "trainschedule-7a996.appspot.com",
+  messagingSenderId: "480470890423"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+//display anything in firebase on page
+
+
+//Capture train info on click
+$("#submit").on("click", function (event) {
+  event.preventDefault();
+  var newTrain = {
+    name: `${$("#train-name").val().trim()}`,
+    dest: `${$("#destination").val().trim()}`,
+    start: `${$("#first-train-time").val()}`,
+    freq: `${$("#frequency").val()}`,
   };
-  firebase.initializeApp(config);
-
-  var database = firebase.database();
-  //display anything in firebase on page
-
-
-  //Capture train info on click
-  $("#submit").on("click", function(event) {
-    event.preventDefault();
-    var newTrain ={
-    trainName: `${$("#train-name").val().trim()}`,
-    destination: `${$("#destination").val().trim()}`,
-    firstTrainTime: `${$("#first-train-time").val()}`,
-    frequency: `${$("#frequency").val()}`,
-    };
-    console.log(newTrain)
-    //Push info into firebase
-    database.ref().push(newTrain);
-    //clear inputs
-    $("input").val("");
-  });
+  console.log(newTrain)
+  //Push info into firebase
+  database.ref().push(newTrain);
+  //clear inputs
+  $("input").val("");
+});
 
 
 //Display on page; displays for every child and then when new child added
-database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+database.ref().on("child_added", function (childSnapshot, prevChildKey) {
+  //static variables
   var train = childSnapshot.val();
-  var trainName = train.trainName;
-  var destination = train.destination;
-  var frequency = train.frequency;
-  var nextArrival;
-  //(moment()- moment(train.firstTrainTime).format("HH:mm"))*frequency + train.firstTrainTime;
-  //console.log('"nextarrival:" ' + nextArrival);
-  //console.log("first train time from now" + train.firstTrainTime.fromNow());
-  //concole.log("first train time in minutes" + train.firstTrainTime.toMinutes());
-  var minutesAway; //moment unis of next arrival - moent unix of now (or jsut diff between)
+  var name = train.name;
+  var dest = train.dest;
+  var freq = train.freq;
+  //start as moment object for use in dynamic variables
+  var start = moment(train.start, "HH:mm");
+  var minFromStart = moment().diff(start, "minutes");
+
+  //dynamic variables
+  var nextTrain;
+  var minTilNext;
+
+//if train has started, find out when the next rain will come
+//else the train will come at it's start time
+//for each, how many minutes until that time
+  if (minFromStart > 0) {
+    nextTrain = start.add(Math.ceil(minFromStart / freq) * freq, "m");
+    minTilNext = Math.abs(moment().diff(nextTrain, "minutes"));
+    nextTrain = nextTrain.format("HH:mm");
+  }
+  else {
+    nextTrain = train.start
+    minTilNext = Math.abs(minFromStart);
+  }
+
+//appending to table
   $("#trains-added").append(`<tr>
-  <td>${trainName}</td>
-  <td>${destination}</td>
-  <td>${frequency}</td>
-  <td>${nextArrival}</td>
-  <td>${minutesAway}</td></tr>`);
-
-console.log("frequency type " + typeof parseInt(frequency));  
-var currentTime = moment() //.format("HH:mm");
-console.log("firstTrainTime " + train.firstTrainTime);
-console.log("firstTrainTime HH:mm " + moment(train.firstTrainTime, "HH:mm").format("HH:mm"));
-var databaseTime = moment("08:00", "HH:mm") //.format("HH:mm");
-
-console.log("diff?" + (moment().diff(databaseTime)).toMinutes());
-
+ <td>${name}</td>
+ <td>${dest}</td>
+ <td>${freq}</td>
+ <td>${nextTrain}</td>
+ <td>${minTilNext + 1}</td></tr>`); //minTilNext off by 1 minute (not sure why) so did +1
 });
- 
-
-
-//on load loop to display stored data, to display stored data is function (call in loop), which can also be called each time train is added
-
-//=====================================================================
